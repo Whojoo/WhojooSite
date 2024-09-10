52ca3de69d54b51e9121c26990f0913c2b659983
Whojoo <de.gier.robin@gmail.com>
2024-09-10 18:47:09 +0200

    Reversed StepId to Step in Recipes since it is an Entity and not an aggregate

--- backend/src/WhojooSite.Recipes/WhojooSite.Recipes.Module/Domain/Recipes/Recipe.cs
+++ backend/src/WhojooSite.Recipes/WhojooSite.Recipes.Module/Domain/Recipes/Recipe.cs
@@ -6,16 +6,16 @@ public sealed class Recipe : Entity<RecipeId>
 {
     private readonly string _name = string.Empty;
     private readonly string _description = string.Empty;
-    private readonly List<StepId> _stepIds = [];
+    private readonly List<Step> _steps = [];
     private readonly Guid _ownerId = Guid.Empty;
 
-    public Recipe(RecipeId id, string name, string description, Guid ownerId, List<StepId> stepIds)
+    public Recipe(RecipeId id, string name, string description, Guid ownerId, List<Step> steps)
     : base(id)
     {
         _name = name;
         _description = description;
         _ownerId = ownerId;
-        _stepIds = stepIds;
+        _steps = steps;
     }
 
     private Recipe() { }
